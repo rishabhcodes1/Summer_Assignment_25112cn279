2beae2476e123f7c4f8cfbabaec53f8204c46594
Rishabh <codewithrishabh01@gmail.com>
2026-06-30 23:55:48 +0530

    Delete Day-15 directory

--- Day-15/q1.py
+++
@@ -1,5 +0,0 @@
-arr = [1, 2, 3, 4, 5]
-
-arr = arr[1:] + arr[:1]
-
-print("After Left Rotation =", arr)
--- Day-15/q2.py
+++
@@ -1,5 +0,0 @@
-arr = [10, 20, 30, 40, 50]
-
-arr.reverse()
-
-print("Reversed Array =", arr)
