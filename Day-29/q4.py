3d5c53b40f08bc5b8944d83606155c4181f98637
Rishabh <codewithrishabh01@gmail.com>
2026-07-01 00:52:44 +0530

    Add files via upload

---
+++ Day-29/q4.py
@@ -0,0 +1,11 @@
+item = input("Enter item name: ")
+quantity = int(input("Enter quantity: "))
+price = float(input("Enter price: "))
+
+total = quantity * price
+
+print("\nInventory Details")
+print("Item :", item)
+print("Quantity :", quantity)
+print("Price :", price)
+print("Total Cost :", total)
