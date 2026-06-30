4100551a183beeb3156429b46b6a4055eb060e31
Rishabh <codewithrishabh01@gmail.com>
2026-07-01 00:02:40 +0530

    Add files via upload

---
+++ Day-17/q1.py
@@ -0,0 +1,7 @@
+arr1 = list(map(int, input("Enter first array: ").split()))
+arr2 = list(map(int, input("Enter second array: ").split()))
+
+merged = arr1 + arr2
+
+print("Merged Array:")
+print(merged)
