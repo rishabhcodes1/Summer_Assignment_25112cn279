248ce3134ee851b513e927790c0a552e23fae1d4
Rishabh <codewithrishabh01@gmail.com>
2026-07-01 00:03:00 +0530

    Add files via upload

---
+++ Day-18/q1.py
@@ -0,0 +1,11 @@
+arr = list(map(int, input("Enter elements: ").split()))
+
+n = len(arr)
+
+for i in range(n):
+    for j in range(0, n-i-1):
+        if arr[j] > arr[j+1]:
+            arr[j], arr[j+1] = arr[j+1], arr[j]
+
+print("Sorted Array:")
+print(arr)
---
+++ Day-18/q4.py
@@ -0,0 +1,6 @@
+arr = list(map(int, input("Enter elements: ").split()))
+
+arr.sort(reverse=True)
+
+print("Descending Order:")
+print(arr)
