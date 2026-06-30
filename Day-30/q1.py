b4989eb38cf69d65e69b193f8986b8188d619a46
Rishabh <codewithrishabh01@gmail.com>
2026-07-01 00:53:14 +0530

    Add files via upload

---
+++ Day-30/q1.py
@@ -0,0 +1,9 @@
+
+name = input("Enter student name: ")
+roll = input("Enter roll number: ")
+marks = int(input("Enter marks: "))
+
+print("\nStudent Record")
+print("Name :", name)
+print("Roll No :", roll)
+print("Marks :", marks)
---
+++ Day-30/q2.py
@@ -0,0 +1,6 @@
+book = input("Enter book name: ")
+author = input("Enter author name: ")
+
+print("\nLibrary Record")
+print("Book :", book)
+print("Author :", author)
